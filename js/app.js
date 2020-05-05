7388ed24bc5e1a22f9375738a985978d762e6477
Sarah Shatto <Sarahshatto@gmail.com>
2020-05-05 00:15:54 -0700

    created directories and added code into js

---
+++ js/app.js
@@ -0,0 +1,72 @@
+'use strict';
+
+var seattleloc = {
+  name: 'Seattle',
+  minCust: 23,
+  maxCust: 65,
+  AvgCookie: 6.3,
+  cookieSalesPerHour: [],
+  
+  getRandomCust: function() {
+    return Math.floor(Math.random() * (maxCust - minCust + 1)) + minCust;
+  },
+  
+  simCookiesPerHour: function() {
+    return AvgCookie * getRandomCust(); // This is total simulated cookies in an hour
+  },
+  
+  simCookiesPerDay : function() {
+  	var hoursOpenPerDay = 14;
+    for(var i = 0; i < hoursOpenPerDay; i++) {
+      cookieSalesPerHour[i] = simCookiesPerHour();
+    },
+    
+  },
+  
+};
+  
+var tokyoloc = {
+  name: 'Tokyo',
+  minCust: 3,
+  maxCust: 24,
+  AvgCookie: 1.2,
+
+  getRandomCust: function() {
+    return Math.floor(Math.random() * (maxCust - minCust + 1)) + minCust;
+  }
+}
+
+var dubailoc = {
+  name: 'Dubai',
+  minCust: 11,
+  maxCust: 38,
+  AvgCookie: 3.7,
+
+  getRandomCust: function() {
+    return Math.floor(Math.random() * (maxCust - minCust + 1)) + minCust;
+  }
+}
+  
+var parisloc = {
+  name: 'Paris',
+  minCust: 20,
+  maxCust: 38,
+  AvgCookie: 2.3,
+
+  getRandomCust: function() {
+    return Math.floor(Math.random() * (maxCust - minCust + 1)) + minCust;
+  }
+}
+
+var limaloc = {
+  name: 'lima',
+  minCust: 2,
+  maxCust: 16,
+  AvgCookie: 4.6,
+
+  getRandomCust: function() {
+    return Math.floor(Math.random() * (maxCust - minCust + 1)) + minCust;
+  }
+} 
+  
+
