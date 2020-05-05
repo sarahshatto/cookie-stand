32184f4b6b842a7e8d31d8f5d63aa7234dd67311
Sarah Shatto <Sarahshatto@gmail.com>
2020-05-05 14:16:35 -0700

    final fixes

--- js/app.js
+++ js/app.js
@@ -12,7 +12,7 @@
 // total cookies for the day ::after
 'use strict';
 
-var hours = ['6am', '7am', '8am', '9am', '10am','11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm',];
+var hours = ['6am', '7am', '8am', '9am', '10am','11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm'];
 
 var seattle = {
   name: 'seattle',
@@ -41,8 +41,8 @@ var seattle = {
     }
   },
   render: function(){
-    seattle.customersEachHour();
-    seattle.cookiesSoldEachHour();
+    seattle.calcCustomersEachHour();
+    seattle.calcCookiesSoldEachHour();
     var seattleElement = document.getElementById('seattle')
     // get the parent element from the DOM
     // 1. Create an Element
