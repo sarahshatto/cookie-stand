c718e91a36e9d37f5cb50372224773b11b061a57
Sarah Shatto <Sarahshatto@gmail.com>
2020-05-06 09:28:12 -0700

    changes to js

--- js/app.js
+++ js/app.js
@@ -14,6 +14,82 @@
 
 var hours = ['6am', '7am', '8am', '9am', '10am','11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm'];
 
+
+
+function Stores(name, minCustomersEachHour, maxCustomersEachHour, averageCookiesSoldPerCustomer, customersEachHour, cookiesSoldEachHour, totalCookiesForTheDay){
+  this.name = name;
+  this.minCustomersEachHour = minCustomersEachHour;
+  this.maxCustomersEachHour = maxCustomersEachHour;
+  this.averageCookiesSoldPerCustomer = averageCookiesSoldPerCustomer;
+  this.customersEachHour = customersEachHour;
+  this.cookiesSoldEachHour = cookiesSoldEachHour;
+  this.totalCookiesForTheDay = totalCookiesForTheDay;
+}
+
+Stores.prototype.getRandomNumber = function(min,max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min; // The max and min are both inclusive
+},
+
+Stores.prototype.calcCustomersEachHour = function(){
+  // calculate the customers each hour // for loop over hours, make a helper function that generates a random number// and push into an array - in a prototype! //
+  for(var i=0; i<hours.length; i++){
+    var customersThisHours = this.getRandomNumber(this.minCustomersEachHour, this.maxCustomersEachHour);
+    this.customersEachHour.push(customersThisHours);
+  }
+},
+
+Stores.prototype.calcCookiesSoldEachHour = function(){
+  // multiply the customers by the average cookies each customer buys - in a prototype! //
+  for(var i=0; i<this.customersEachHour.length; i++){
+    var wholeCookiesSoldForOneHour = Math.ceil(this.customersEachHour[i] * this.averageCookiesSoldPerCustomer);
+
+    this.cookiesSoldEachHour.push(wholeCookiesSoldForOneHour);
+
+    this.totalCookiesForTheDay += wholeCookiesSoldForOneHour; // this will take the total cookies and add the variable
+  }
+},
+
+Stores.prototype.render = function(){
+  this.calcCustomersEachHour();
+  this.calcCookiesSoldEachHour();
+  var elementish = document.getElementById(this.name);
+  var listItem = document.createElement('li');
+  listItem.textContent = this.name;
+  elementish.appendChild(listItem);
+  // get the parent element from the DOM
+  // 1. Create an Element
+  // 2. fill it with text content
+  // 3. Append
+  // render the name of the store
+
+  //render cookiesSoldEachHour
+  for(var i=0; i<hours.length; i++){
+    listItem = document.createElement('li');
+    listItem.textContent = `${hours[i]}: ${this.cookiesSoldEachHour[i]} cookies`;
+    elementish.appendChild(listItem);
+  }
+  // this will render totalCookiesForTheDayToTheDOM
+  listItem = document.createElement('li');
+  listItem.textContent = this.totalCookiesForTheDay;
+  elementish.appendChild(listItem);
+};
+
+var Seattle = new Stores('Seattle', 23, 65, 6.3, [], [], 0);
+var Tokyo = new Stores('Tokyo', 3, 24, 1.2, [], [], 0);
+var Dubai = new Stores('Dubai', 3, 24, 1.2, [], [], 0);
+var Paris = new Stores('Paris', 20, 38, 2.3, [], [], 0);
+var Lima = new Stores('Lima', 2, 16, 4.6, [], [], 0);
+
+
+// seattle.calcCustomersEachHour();
+// seattle.calcCookiesSoldEachHour();
+// seattle.getRandomNumber();
+Seattle.render();
+Tokyo.render();
+Dubai.render();
+Paris.render();
+Lima.render();
+
 // var seattle = {
 //   name: 'Seattle',
 //   minCustomersEachHour: 23,
@@ -76,82 +152,6 @@ var hours = ['6am', '7am', '8am', '9am', '10am','11am', '12pm', '1pm', '2pm', '3
 
 ///// attempt at a constructor function
 ///////// FEEDBACK: Possibly remove customersEachHour, cookiesSoldEachHour, totalCookiesForTheDay from the parameters and the actual object since those values are defined in the other functions. Recommended that I either remove them or set them to a default value like 0.
-
-function Location(name, minCustomersEachHour, maxCustomersEachHour, averageCookiesSoldPerCustomer, customersEachHour, cookiesSoldEachHour, totalCookiesForTheDay){
-  this.name = name;
-  this.minCustomersEachHour = minCustomersEachHour;
-  this.maxCustomersEachHour = maxCustomersEachHour;
-  this.averageCookiesSoldPerCustomer = averageCookiesSoldPerCustomer;
-  this.customersEachHour = customersEachHour;
-  this.cookiesSoldEachHour = cookiesSoldEachHour;
-  this.totalCookiesForTheDay = totalCookiesForTheDay;
-}
-
-Location.prototype.getRandomNumber = function(min,max) {
-  return Math.floor(Math.random() * (max - min + 1)) + min; // The max and min are both inclusive
-},
-
-Location.prototype.calcCustomersEachHour = function(){
-  // calculate the customers each hour and place in array - in a prototype! //
-  for(var i=0; i<hours.length; i++){
-    var customersThisHours = this.getRandomNumber(this.minCustomersEachHour, this.maxCustomersEachHour);
-    this.customersEachHour.push(customersThisHours);
-  }
-},
-
-Location.prototype.calcCookiesSoldEachHour = function(){
-  // multiply the customers by the average cookies each customer buys - in a prototype! //
-  for(var i=0; i<this.customersEachHour.length; i++){
-    var wholeCookiesSoldForOneHour = Math.ceil(this.customersEachHour[i] * this.averageCookiesSoldPerCustomer);
-
-    this.cookiesSoldEachHour.push(wholeCookiesSoldForOneHour);
-
-    this.totalCookiesForTheDay += wholeCookiesSoldForOneHour; // this will take the total cookies and add the variable
-  }
-},
-
-Location.prototype.render = function(){
-  this.calcCustomersEachHour();
-  this.calcCookiesSoldEachHour();
-  var elementish = document.getElementById(this.name);
-  var listItem = document.createElement('li');
-  listItem.textContent = this.name;
-  elementish.appendChild(listItem);
-  // get the parent element from the DOM
-  // 1. Create an Element
-  // 2. fill it with text content
-  // 3. Append
-  // render the name of the store
-
-  //render cookiesSoldEachHour
-  for(var i=0; i<hours.length; i++){
-    listItem = document.createElement('li');
-    listItem.textContent = `${hours[i]}: ${this.cookiesSoldEachHour[i]} cookies`;
-    elementish.appendChild(listItem);
-  }
-  // this will render totalCookiesForTheDayToTheDOM
-  listItem = document.createElement('li');
-  listItem.textContent = this.totalCookiesForTheDay;
-  elementish.appendChild(listItem);
-};
-
-var Seattle = new Location('Seattle', 23, 65, 6.3, [], [], 0);
-var Tokyo = new Location('Tokyo', 3, 24, 1.2, [], [], 0);
-var Dubai = new Location('Dubai', 3, 24, 1.2, [], [], 0);
-var Paris = new Location('Paris', 20, 38, 2.3, [], [], 0);
-var Lima = new Location('Lima', 2, 16, 4.6, [], [], 0);
-
-
-// seattle.calcCustomersEachHour();
-// seattle.calcCookiesSoldEachHour();
-// seattle.getRandomNumber();
-Seattle.render();
-Tokyo.render();
-Dubai.render();
-Paris.render();
-Lima.render();
-
-
 
 ////////////////////////////////////////
 ////////////////// Tokyo Location
